809b5f5e60c5b5ed9235b2f7b7faf2f303a3847b
Vasishthbhatt <vbbhatt333@gmail.com>
2023-02-27 18:57:55 +0530

    Js validaton and look changes
    I have done some changes in background and applied some js validations by myself

--- Login/signup.php
+++ Login/signup.php
@@ -3,21 +3,28 @@
 <head>
 	<title>Sign Up Page</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+    <script src="validation.js"></script>
 </head>
 <body>
+   
 	<div class="signup-box">
 		<h2>Sign Up Here</h2>
-		<form action="signup.php" method="POST">
+		<form name = "signupForm" action="signup.php" method="POST" onsubmit="return validateSignupForm()">
 			<p>Username:</p>
-			<input type="text" name="username" placeholder="Enter Username">
+			<input type="text" name="username" id="username" value ="" placeholder="Enter Username">
+			<p id="username_err">Username cannot be empty</p>
 			<p>Email:</p>
-			<input type="email" name="email" placeholder="Enter Email">
+			<input type="email" name="email" id = "email" value="" placeholder="Enter Email">
 			<p>Password:</p>
-			<input type="password" name="password" placeholder="Enter Password">
-			<input type="submit" name="submit" value="Sign Up">
+			<input type="password" name="password" id="password" value="" placeholder="Enter Password">
+			<p id="pass_err">
+				Password must contain atleast on Capital letter. <br>
+				Password must contain atleast one Number. <br>
+				Password must be greater than 7 charachters. <br>
+				Password must contain atleast one special charachter.<br>
+			</p>
+			<input type="submit" name="submit" value="Sign Up" >
 			<p>Already have an account? <a href="login.php">Login</a></p>
-
-			
 		</form>
 	</div>
 </body>
--- Login/validation.js
+++ Login/validation.js
@@ -1,16 +1,48 @@
 function validateLoginForm(){
     var username = document.forms["loginForm"]["username"].value;
     var password = document.forms["loginForm"]["password"].value;
+ 
     if (username == "") {
         alert("Please enter your username");
         return false;
     }
-    if (password == "") {
+    if (password == "" ) {
         alert("Please enter your password");
         return false;
     }
 }
 
 function validateSignupForm(){
-    var username = document.forms["signupForm"]["username"].value;
-   
+    let username = document.forms["signupForm"]["username"];
+    let password = document.forms["signupForm"]["password"];
+    let pass_err = document.getElementById("pass_err");
+    let username_err = document.getElementById("username_err");
+    let email = document.forms["signupForm"]["email"];
+    let strongPassword = new RegExp('(?=^.{8,}$)(?=.*\\d)(?=.*[!@#$%^&*]+)(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$');
+    let validator = true;
+    console.log(password.value)
+    if(username.value == ""){
+        username.style.boxShadow ="0px 2px 0px  rgba(255, 24, 24, 0.6)";
+        username.style.border ="2px solid red"; 
+        username_err.style.display ="block";
+        validator= false;
+        
+    }
+    console.log(validator);
+    if(email.value == ""){
+        validator= false;
+    }
+    console.log(validator);
+    if(!strongPassword.test(password.value)){
+        password.style.boxShadow ="0px 2px 0px  rgba(255, 24, 24, 0.6)";
+        password.style.border ="2px solid red";
+        pass_err.style.display = "block";
+        validator =  false;    
+    }
+    console.log(validator);
+    if(validator){
+        alert("Submission Sucessful")
+        return true;
+    }
+    else{return false;}
+}   
